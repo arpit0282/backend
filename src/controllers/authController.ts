20e15a80e2c8b2e492123852e0e672a49fa56544
arpit0282 <arpit8345@gmail.com>
2024-09-27 07:48:02 +0530

    Add files via upload

---
+++ src/controllers/authController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import User from '../model/user';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export const register = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const user = await User.create({ username, password: hashedPassword });
+    res.status(201).json({ id: user.id, username: user.username });
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating user' });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ where: { username } });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+      res.json({ token });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error logging in' });
+  }
+};
---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.header('Authorization')?.split(' ')[1];
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET!, (err: any, user: any) => {
+      if (err) return res.sendStatus(403);
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
